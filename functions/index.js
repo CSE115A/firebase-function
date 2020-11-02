df1675c97bc5cb806ea08aaa1fbcdf60f2c53df4
ilopezro <ilopezro@ucsc.edu>
2020-11-01 23:53:26 -0800

    wip --switching to laptop

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,8 @@
 const functions = require("firebase-functions");
+require("isomorphic-fetch");
 
-exports.getPrices = functions.https.onRequest((request, response) => {});
+exports.getPrices = functions.https.onRequest((request, response) => {
+  return fetch("https://www.lyft.com/api/costs")
+    .then((res) => response.status(200).send(res.message))
+    .catch((error) => response.status(500).send(error.message));
+});
