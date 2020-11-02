753da63ac20c2b3dfe71ef9da621d3b82b34fe32
Isai Lopez <ilopezro@ucsc.edu>
2020-11-02 00:36:45 -0800

    got general query down

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require("firebase-functions");
-require("isomorphic-fetch");
+const axios = require("axios");
 
-exports.getPrices = functions.https.onRequest((request, response) => {
-  return fetch("https://www.lyft.com/api/costs")
-    .then((res) => response.status(200).send(res.message))
-    .catch((error) => response.status(500).send(error.message));
+exports.getPrices = functions.https.onRequest(async (request, response) => {
+  const lyftEndpoint = "https://www.lyft.com/api/costs";
+  const configHeaders = { headers: { Host: "www.lyft.com" } };
+  return await axios
+    .get(
+      lyftEndpoint,
+      {
+        params: {
+          start_lat: 38.0067935,
+          start_lng: -122.5496167,
+          end_lat: 37.9742222,
+          end_lng: -122.5329032,
+        },
+      },
+      configHeaders
+    )
+    .then((res) => {
+      return response.status(200).send({
+        error: false,
+        status: 200,
+        price_estimates: res.data.cost_estimates,
+      });
+    })
+    .catch((err) => {
+      return response.send(err);
+    });
 });
